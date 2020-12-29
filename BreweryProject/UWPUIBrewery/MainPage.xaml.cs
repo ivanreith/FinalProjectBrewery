ec3997a72329178b958a38a3e5ad08adf2e58c0f
ivanreith <castronunoi@my.lanecc.edu>
2020-12-28 17:12:04 -0800

    Got to start the UI, added functions, search working fine, added delete create and edit, testing next...

--- BreweryProject/UWPUIBrewery/MainPage.xaml.cs
+++ BreweryProject/UWPUIBrewery/MainPage.xaml.cs
@@ -31,7 +31,18 @@ namespace UWPUIBrewery
     {
         private Supplier selected = null;
         private IngredientInventoryAddition ingredientAdded;
+
+        private Ingredient ingredient;
+       
         private HttpDataService service;
+      
+
+        public Ingredient Ingredient
+        {
+            get { return ingredient; }
+            set { ingredient = value; }
+        }
+      
         public Supplier Selected
         {
             get { return selected; }
@@ -40,9 +51,10 @@ namespace UWPUIBrewery
         public IngredientInventoryAddition IngredientAdded
         {
             get { return ingredientAdded; }
-            set { ingredientAdded = value; }
+            set { ingredientAdded = value; }// I don't think I need to modify it.
         }
-        public ObservableCollection<Supplier> Suppliers { get; private set; } = new ObservableCollection<Supplier>();
+        public ObservableCollection<Ingredient> Ingredients { get; private set; } = new ObservableCollection<Ingredient>();
+              public ObservableCollection<Supplier> Suppliers { get; private set; } = new ObservableCollection<Supplier>();
         public ObservableCollection<IngredientInventoryAddition> IngredientsAdditions { get; private set; } = new ObservableCollection<IngredientInventoryAddition>();
 
         public MainPage()
@@ -55,9 +67,11 @@ namespace UWPUIBrewery
             List<Supplier> suppliers = await service.GetAsync<List<Supplier>>("Suppliers");
             foreach (Supplier s in suppliers)
                 this.Suppliers.Add(s);
-            //ClearCustomerDetails();
-           // EnableFields(false);
-           // EnableButtons("pageLoad");
+          
+            ClearSupplierDetails();
+            EnableFields(false);
+            this.supplierIdTxt.IsEnabled = true;
+            EnableButtons("pageLoad");
         }
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
@@ -72,30 +86,61 @@ namespace UWPUIBrewery
             // editing
             else
             {
-                DisplayProviderDetails();
+                DisplaySupplierDetails();
                 this.supplierIdTxt.IsEnabled = true;
                 EnableButtons("found");
             }
 
         }
 
-        private void DisplayProviderDetails()
+        private void DisplaySupplierDetails()
         {
-            this.supplierIdTxt.Text = Selected.SupplierId.ToString();
-            this.supplierNameTxt.Text = Selected.Name;
-            this.supplierAddressTxt.Text = Selected.SupplierAddress.ToString();
-            this.supplierPhoneTxt.Text = Selected.Phone;
-            this.supplierWebsiteTxt.Text = Selected.Website;
-            this.supplierContactPersonNameTxt.Text = Selected.ContactFirstName + Selected.ContactLastName;
-           // int stateIndex = this.FindStateIndex(Selected.StateCode);
-           // this.customerStateCBox.SelectedIndex = stateIndex;
+            if (Selected.SupplierId.ToString() != null)
+                this.supplierIdTxt.Text = Selected.SupplierId.ToString();
+            else
+                this.supplierIdTxt.Text = "";
+            if (Selected.Name != null)
+                this.supplierNameTxt.Text = Selected.Name;
+            else
+                this.supplierNameTxt.Text = "";
+            // this.supplierAddressTxt.Text = Selected.SupplierAddress.ToString();
+            if (Selected.Phone != null)
+                this.supplierPhoneTxt.Text = Selected.Phone;
+            else
+                this.supplierPhoneTxt.Text = "";
+            if (Selected.Email != null)
+                this.supplierEmailTxt.Text = Selected.Email;
+            else
+                this.supplierEmailTxt.Text = "";
+            if (Selected.Website != null)
+                this.supplierWebsiteTxt.Text = Selected.Website;
+            else
+                this.supplierWebsiteTxt.Text = "";
+            if (Selected.ContactFirstName != null)
+                this.supplierContactPersonNameTxt.Text = Selected.ContactFirstName;
+            else
+                this.supplierContactPersonNameTxt.Text = "";
+            if (Selected.ContactLastName != null)
+                this.supplierContactPersonLastNameTxt.Text = Selected.ContactLastName;
+            else
+                this.supplierContactPersonLastNameTxt.Text = "";
+            if (Selected.ContactPhone != null)
+                this.supplierContactPhoneNumberTxt.Text = Selected.ContactPhone;
+            else
+                this.supplierContactPhoneNumberTxt.Text = "";
+            if (Selected.ContactEmail != null)
+                this.supplierContactEmailTxt.Text = Selected.ContactEmail;
+            else
+                this.supplierContactEmailTxt.Text = "";
+            // int stateIndex = this.FindStateIndex(Selected.StateCode);
+            // this.customerStateCBox.SelectedIndex = stateIndex;
             EnableFields(false);
+            this.supplierIdTxt.IsEnabled = true;
         }
         private void ClearSupplierDetails()
         {
             this.supplierIdTxt.Text = "";
-            this.supplierNameTxt.Text = "";
-            this.supplierAddressTxt.Text = "";
+            this.supplierNameTxt.Text = "";         
             this.supplierPhoneTxt.Text = "";
             this.supplierEmailTxt.Text = "";
             this.supplierWebsiteTxt.Text = "";
@@ -103,31 +148,34 @@ namespace UWPUIBrewery
             this.supplierContactPersonLastNameTxt.Text = "";
             this.supplierContactPhoneNumberTxt.Text = "";
             this.supplierContactEmailTxt.Text = "";
-            EnableFields(false);
+            EnableFields(true);
+           // this.supplierIdTxt.IsEnabled = true;
         }
         private void EnableFields(bool enabled = true)
         {
             this.supplierIdTxt.IsEnabled = enabled;
-            this.supplierNameTxt.IsEnabled = enabled;
-            this.supplierAddressTxt.IsEnabled = enabled;
+            this.supplierNameTxt.IsEnabled = enabled;           
+            this.supplierPhoneTxt.IsEnabled = enabled;
             this.supplierEmailTxt.IsEnabled = enabled;
             this.supplierWebsiteTxt.IsEnabled = enabled;
             this.supplierContactPersonNameTxt.IsEnabled = enabled;
             this.supplierContactPersonLastNameTxt.IsEnabled = enabled;
             this.supplierContactPhoneNumberTxt.IsEnabled = enabled;
             this.supplierContactEmailTxt.IsEnabled = enabled;
+            
+            this.supplierIdTxt.SelectAll();
         }
         private void EnableButtons(string state)
         {
             switch (state)
             {
                 case "pageLoad":
-                    this.SearchSupplierBtn.IsEnabled = false;
+                    this.SearchSupplierBtn.IsEnabled = true;
                     this.EditSupplierBtn.IsEnabled = false;
                     this.DeleteSupplierBtn.IsEnabled = false;
-                    this.CreateSupplierBtn.IsEnabled = false;
+                    this.CreateSupplierBtn.IsEnabled = true;
                     this.OrderHistoryBtn.IsEnabled = false;
-                    this.CancelBtn.IsEnabled = true;
+                    this.CancelBtn.IsEnabled = false;
                     this.SaveSupplierBtn.IsEnabled = false;
                     break;
                 case "editing":
@@ -145,7 +193,7 @@ namespace UWPUIBrewery
                     this.DeleteSupplierBtn.IsEnabled = true;
                     this.EditSupplierBtn.IsEnabled = true;
                     this.CreateSupplierBtn.IsEnabled = false;
-                    this.SearchSupplierBtn.IsEnabled = false;
+                    this.SearchSupplierBtn.IsEnabled = true;
                     this.SaveSupplierBtn.IsEnabled = false;
                     this.CancelBtn.IsEnabled = false;
                     break;
@@ -157,43 +205,130 @@ namespace UWPUIBrewery
             if (Selected != null)
             {
                 int supplierId = Selected.SupplierId;
-                if (await service.DeleteAsync("customers\\" + supplierId))
+                if (await service.DeleteAsync("suppliers\\" + supplierId))
                 {
                     Selected = null;
                     this.supplierIdTxt.Text = "";
                     ClearSupplierDetails();
                     EnableButtons("pageLoad");
-                    var messageDialog = new MessageDialog("Customer was deleted.");
+                    var messageDialog = new MessageDialog("Supplier was deleted.");
                     await messageDialog.ShowAsync();
                 }
                 else
                 {
-                    var messageDialog = new MessageDialog("There was a problem deleting that Provider.");
+                    var messageDialog = new MessageDialog("There was a problem deleting that Supplier.");
                     await messageDialog.ShowAsync();
                 }
             }
-        }
+        } // END DELETE
         private void CreateSupplier_Click(object sender, RoutedEventArgs e)
-        { 
-        
-        }
-
-        private void SaveSupplier_Click(object sender, RoutedEventArgs e)
         {
+            Selected = null;
+            ClearSupplierDetails();
+            this.supplierIdTxt.Text = "";    
+            EnableFields(true);
+            this.supplierIdTxt.IsEnabled = false;
+            EnableButtons("adding");
+        }// END CREATE
 
-        }
-        private void SearchSupplier_Click(object sender, RoutedEventArgs e)
+        private async void SaveSupplier_Click(object sender, RoutedEventArgs e)
         {
+            // adding
+            if (Selected == null)
+            {
+                Supplier newSupplier = new Supplier();
+                newSupplier.Name = this.supplierNameTxt.Text;
+                newSupplier.Phone = this.supplierPhoneTxt.Text;
+                newSupplier.Email = this.supplierEmailTxt.Text;
+                newSupplier.Website = this.supplierWebsiteTxt.Text;
+                newSupplier.ContactFirstName = this.supplierContactPersonNameTxt.Text;
+                newSupplier.ContactLastName = this.supplierContactPersonLastNameTxt.Text;                
+                newSupplier.ContactPhone = this.supplierContactPhoneNumberTxt.Text;
+                newSupplier.ContactEmail = this.supplierContactEmailTxt.Text;
+                HttpResponseMessage response = await service.PostAsJsonAsync<Supplier>("suppliers", newSupplier, true);
+                if (response.IsSuccessStatusCode)
+                {
+                    // the customer id is at the end of response.Headers.Location.AbsolutePath
+                    string url = response.Headers.Location.AbsolutePath;
+                    int index = url.LastIndexOf("/");
+                    string supplierId = url.Substring(index + 1);
+                    newSupplier.SupplierId = int.Parse(supplierId);
+                    Selected = newSupplier;
+                    this.supplierIdTxt.Text = Selected.SupplierId.ToString();
+                    this.supplierIdTxt.IsEnabled = true;
+                    DisplaySupplierDetails();
+                    EnableButtons("found");
+                    var messageDialog = new MessageDialog("Supplier was added.");
+                    await messageDialog.ShowAsync();
+                }
+                else
+                {
+                    var messageDialog = new MessageDialog("There was a problem adding that Supplier.");
+                    await messageDialog.ShowAsync();
+                }
+            }
+            // editing
+            else
+            {
+                Supplier updatedSupplier = new Supplier();
+                updatedSupplier.SupplierId = Selected.SupplierId;
+                updatedSupplier.Name = this.supplierNameTxt.Text;
+                updatedSupplier.Phone = this.supplierPhoneTxt.Text;
+                updatedSupplier.Email = this.supplierEmailTxt.Text;
+                updatedSupplier.Website = this.supplierWebsiteTxt.Text;
+                updatedSupplier.ContactFirstName = this.supplierContactPersonNameTxt.Text;
+                updatedSupplier.ContactLastName = this.supplierContactPersonLastNameTxt.Text;
+                updatedSupplier.ContactPhone = this.supplierContactPhoneNumberTxt.Text;
+                updatedSupplier.ContactEmail = this.supplierContactEmailTxt.Text;
 
+                if (await service.PutAsJsonAsync<Supplier>("suppliers\\" + updatedSupplier.SupplierId, updatedSupplier))
+                {
+                    Selected = updatedSupplier;
+                    DisplaySupplierDetails();
+                    this.supplierIdTxt.IsEnabled = true;
+                    EnableButtons("found");
+                    var messageDialog = new MessageDialog("Supplier was updated.");
+                    await messageDialog.ShowAsync();
+                }
+                else
+                {
+                    var messageDialog = new MessageDialog("There was a problem updating that Supplier.");
+                    await messageDialog.ShowAsync();
+                }
+            }
+        }// End SAVE
+        private async void SearchSupplier_Click(object sender, RoutedEventArgs e)
+        {
+            string supplierId = this.supplierIdTxt.Text;
+            try
+            {
+                Selected = await service.GetAsync<Supplier>("suppliers\\" + supplierId, null, true);
+               
+                DisplaySupplierDetails();
+                EnableButtons("found");
+
+            }
+            catch
+            {
+                var messageDialog = new MessageDialog("A Supplier with that Supplier id cannot be found.");
+                await messageDialog.ShowAsync();
+                Selected = null;
+                ClearSupplierDetails();
+                EnableFields(false);
+                this.supplierIdTxt.IsEnabled = true;
+            }
         }
         private void OrderHistory_Click(object sender, RoutedEventArgs e)
         {
 
-        }
+        }//END ORDER HISTORY
         private void EditSupplier_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+            
+            EnableFields(true);
+            this.supplierIdTxt.IsEnabled = false;
+            EnableButtons("editing");
+        } //END EDIT
         private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
         {
 
